af1b1e64b607597e0ab74652fc183a6a13b64ada
Viktor Drozdov <drozdovv88@yandex.ru>
2023-04-03 23:58:00 +0300

    Завершение

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+// Открытие/закрытие PopUp
+const editBtn = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-button');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+
+// Отправка формы
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('#name-input');
+const jobInput = formElement.querySelector('#job-input')
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
+
+function submitHandleForm (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', submitHandleForm);
