a72660a702baf42b27d190ebf4d77bef3acf435f
Viktor Drozdov <drozdovv88@yandex.ru>
2023-04-04 23:17:58 +0300

    Исправления по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
-// Открытие/закрытие PopUp
 const editBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('#name-input');
+const jobInput = formElement.querySelector('#job-input')
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
+
+// Открытие/закрытие PopUp
 function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -15,20 +23,11 @@ editBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 
 // Отправка формы
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('#name-input');
-const jobInput = formElement.querySelector('#job-input')
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
 function submitHandleForm (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
 formElement.addEventListener('submit', submitHandleForm);
